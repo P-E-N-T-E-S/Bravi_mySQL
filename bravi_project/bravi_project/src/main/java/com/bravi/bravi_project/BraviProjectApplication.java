c9baa160daaa8555a3730ee167a78158a93e2900
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2024-10-23 17:24:46 -0300

    creating project

---
+++ bravi_project/bravi_project/src/main/java/com/bravi/bravi_project/BraviProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bravi.bravi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraviProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraviProjectApplication.class, args);
+	}
+
+}
