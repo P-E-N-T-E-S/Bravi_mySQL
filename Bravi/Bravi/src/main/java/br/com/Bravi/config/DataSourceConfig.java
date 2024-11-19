714d011451e12aaf4dbc24db912931bd2b4033a4
Thomazrlima <thomazlima211@gmail.com>
2024-11-19 16:48:59 -0300

    Fazendo rodar no terminal

--- Bravi/Bravi/src/main/java/br/com/Bravi/config/DataSourceConfig.java
+++ Bravi/Bravi/src/main/java/br/com/Bravi/config/DataSourceConfig.java
@@ -5,18 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
-    @Value("${my.app.datasource_url}")
-    private String datasourceUrl;
-    @Value("${my.app.datasource_username}")
-    private String datasourceUsername;
-    @Value("${my.app.datasource_password}")
-    private String datasourcePassword;
+    Dotenv dotenv = Dotenv.load();
+
+    private String datasourceUrl = dotenv.get("DATABASE_URL");
+    private String datasourceUsername = dotenv.get("DATABASE_USERNAME");
+    private String datasourcePassword = dotenv.get("DATABASE_PASSWORD");
 
     @Bean
     public DataSource dataSource() {
